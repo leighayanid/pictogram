31d1e59b71b9f39c49922a678689971fb79ac73f
Leigh Dinaya <leighdinaya04@gmail.com>
2017-01-26 07:55:11 +0800

    add form and edit actions for photo controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,35 @@
 class PhotosController < ApplicationController
+
   def index
+  	@photos = Photo.all
   end
 
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
   end
 
   def delete
   end
+
+  def destroy
+  end
+
+  private
+
+  def find_photo
+  	@photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+  	params.require(:pic).permit(:title, :description)
+  end
+
 end
