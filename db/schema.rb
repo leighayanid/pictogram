7750ad06b0ace41cd879bd535a5065b5c248d0ed
Leigh Dinaya <leighdinaya04@gmail.com>
2017-01-26 07:41:38 +0800

    generate model for photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121234009) do
+ActiveRecord::Schema.define(version: 20170125233711) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
