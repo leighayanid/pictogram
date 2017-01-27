c5d3990de0837a04549b5ec0060f630d3854cf3e
Leigh Dinaya <leighdinaya04@gmail.com>
2017-01-27 09:35:08 +0800

    add association to user and photos

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -3,7 +3,9 @@ class SetupController < ApplicationController
   layout :false
 
   def index
-   
+   if user_signed_in?
+   		redirect_to photos_path
+   	end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125233711) do
+ActiveRecord::Schema.define(version: 20170127011548) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
