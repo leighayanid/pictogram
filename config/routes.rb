7872cee4dee26b911b4a2cb2f2201745e401ccf1
Leigh Dinaya <leighdinaya04@gmail.com>
2017-02-02 18:57:04 +0800

    add resources for upvote inside routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-	resources :photos
+	resources :photos do
+		members do
+			put "like", to: "photo#upvote"
+		end
+	end
   devise_for :users, :controllers => { }
   root 'setup#index'
   get '/setup' => 'setup#index'
