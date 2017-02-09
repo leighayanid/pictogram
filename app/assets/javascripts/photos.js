8ada3ee9b93ecf01fb79400393da641ece13304e
Leigh Dinaya <leighdinaya04@gmail.com>
2017-02-09 08:58:08 +0800

    configure maximage

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,9 +1,23 @@
-$(function(){
-  $('#masonry-container').masonry({
-    itemSelector: '.box',
-    columnWidth: 200,
-    isAnimated: !Modernizr.csstransitions,
-    isFitWidth: true
-  });
+$(function() {
+    $('#masonry-container').masonry({
+        itemSelector: '.box',
+        columnWidth: 200,
+        isAnimated: !Modernizr.csstransitions,
+        isFitWidth: true
+    });
+
+    $('#maximage').maximage({
+        cycleOptions: {
+            fx: 'scrollHorz',
+            speed: 800,
+            timeout: 8000,
+            prev: '#arrow_left',
+            next: '#arrow_right'
+        },
+        onFirstImageLoaded: function() {
+            jQuery('#cycle-loader').hide();
+            jQuery('#maximage').fadeIn('fast');
+        }
+    });
 
 });
