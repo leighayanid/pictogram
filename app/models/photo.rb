5a24826f5610a07e43dd65913515af8ae2cabb11
Leigh Dinaya <leighdinaya04@gmail.com>
2017-02-24 07:46:01 +0800

    configure cloudinary for uploading image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,10 @@
 class Photo < ApplicationRecord
 	acts_as_votable
 	belongs_to :user
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, :storage => :cloudinary, 
+		:path => ":id/:style/:filename",
+		:cloudinary_resource_type => :image, 
+		styles: { medium: "300x300>", thumb: "100x100>" }, 
+		default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
