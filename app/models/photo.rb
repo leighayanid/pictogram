f89425db39b26bde22cb17845e6fd681307aeb4a
Leigh Dinaya <leighdinaya04@gmail.com>
2017-02-24 08:06:06 +0800

    set validate_media_type to false to avoid spoofing issue when uploading

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,7 @@ class Photo < ApplicationRecord
 		:path => ":id/:style/:filename",
 		:cloudinary_resource_type => :image, 
 		styles: { medium: "300x300>", thumb: "100x100>" }, 
-		default_url: "/images/:style/missing.png"
+		default_url: "/images/:style/missing.png",
+		validate_media_type: false
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
