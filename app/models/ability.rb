c0ca2199b1e5c3a3ce15efa66b95120e952ec663
Leigh Dinaya <leighdinaya04@gmail.com>
2017-04-17 18:33:33 +0800

    restrict user to upvote photo

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,12 @@ class Ability
 
   def initialize(user)
   	user || User.new
-    # if user && user.admin?
-    #   can :manage, :all
-    # end
     if user.admin?
     	can :manage, :all
     else
     	can :read, :all
     	can :create, Photo
+        can :upvote, Photo
     	# check if the user is associated with the photo
     	can :update, Photo do |photo|
     		photo.user == user
