c6a04668c1eb776147bea9088fac9d00cd64ac65
Leigh Dinaya <leighdinaya04@gmail.com>
2017-04-22 19:45:15 +0800

    configure permitted parameter for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate
+  before_action :configure_permitted_parameters
 
   def authenticate
     unless ENV['HTTP_AUTH_USERNAME'].blank? or ENV['HTTP_AUTH_PASSWORD'].blank?
@@ -13,6 +14,14 @@ class ApplicationController < ActionController::Base
 
   def access_denied(exception)
     redirect_to root_path, alert: exception.message
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:first_name, :last_name, :username, :birthday, :email, :password, :remember_me)}
+    devise_parameter_sanitizer.for(:sign_in){ |u| u.permit(:first_name, :last_name, :username, :birthday, :email, :password, :remember_me)}
+    devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:first_name, :last_name, :username, :address, :bio, :birthday, :email, :password, :remember_me)}
   end
 
 end
