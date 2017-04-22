ca3a64870c5d3919289f4075e5dd748a707f4d36
Leigh Dinaya <leighdinaya04@gmail.com>
2017-04-22 19:36:23 +0800

    create migration for adding extra detail info to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421020806) do
+ActiveRecord::Schema.define(version: 20170422113144) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "title"
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define(version: 20170421020806) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "role"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "bio"
+    t.date     "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
