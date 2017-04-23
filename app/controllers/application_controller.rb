f11b3626e5f141fad4af53102f86f629afba0310
Leigh Dinaya <leighdinaya04@gmail.com>
2017-04-23 20:09:05 +0800

    edit configure permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate
-  before_action :configure_permitted_parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def authenticate
     unless ENV['HTTP_AUTH_USERNAME'].blank? or ENV['HTTP_AUTH_PASSWORD'].blank?
@@ -19,9 +19,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :birthday, :email, :password, :remember_me)}
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :username, :birthday, :email, :password, :remember_me)}
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :address, :bio, :birthday, :email, :password, :remember_me)}
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :birthday, :email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name, :username, :birthday, :email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :address, :bio, :birthday, :email, :password, :remember_me])
   end
 
 end
