77b5938991a493ecd1e999c25859d5c8d85c0300
Leigh Dinaya <leighdinaya04@gmail.com>
2017-05-11 12:36:19 +0800

    edit user search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	load_and_authorize_resource through: :current_user
+	load_and_authorize_resource through: :current_user, except: [:index, :show]
 	load_and_authorize_resource param_methods: :user_params
 
 	def index
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { }
   resources :users, only: [:index, :show]
   get '/setup' => 'setup#index'
+  get '/photographers' => 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
