e14acd6a7f3c46896f17d3199c70fb355d1981e4
Leigh Dinaya <leighdinaya04@gmail.com>
2017-05-11 12:13:12 +0800

    search users and edit views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,16 @@ class UsersController < ApplicationController
 	load_and_authorize_resource param_methods: :user_params
 
 	def index
-		
+		@q = User.ransack(params[:q])
+		@users = @q.result(distinct: true)
 	end
 
 	def show
+		
+	end
+
+	def user_params
+		params.require(:user).permit(:first_name)
 	end
 
 end
